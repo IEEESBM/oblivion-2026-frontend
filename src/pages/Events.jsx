aff9b7ff5f38ad8fe56337e7c528234c38c50829
Pia Jemin Mehta <piajeminmehta@gmail.com>
2026-04-10 11:07:56 +0530

    updated gallery images and navbar UI

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -731,7 +731,7 @@ const Events = ({ toggleLoginSignup }) => {
               <div style={{ display: "flex" }}>
                 Registered |
                 <a
-                  href="https://chat.whatsapp.com/BbCbwvsKCLZ2qO5iDk6eaZ"
+                  href="https://chat.whatsapp.com/LWgiPYFRHCgEpomIXW2EEO"
                   target="_blank"
                   rel="noopener noreferrer"
                   style={{
